4b2e6fca8ea5604b7a171eecd47905710984b4d9
Amir Safoev <amirsafoev0@gmail.com>
2018-10-27 10:57:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def reformat_languages(languages)
+  new_hash = {}
+  #binding.pry
+  languages.each do |style, language|
+   #amir =  language.values << {:style => [:oo]}
+    #puts amir
+    language.keys.each do |keys|
+      keys[:style] = [[:oo]]
+     
+binding.pry
   # your code here
 end
+binding.pry
+end
+end
+
